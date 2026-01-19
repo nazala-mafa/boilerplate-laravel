c1a9a6dd36e8138d0a44d844af9ddb38a9daecbb
nazala-mafa <thenazala@gmail.com>
2026-01-19 11:25:30 +0700

    feat: add user api test

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::apiResource('product', ProductController::class)->names('product');
+Route::apiResource('user', UserController::class)->names('user');
