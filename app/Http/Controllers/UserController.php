8609abfa1905b70c97682afedef4a1c5a125c676
nazala-mafa <thenazala@gmail.com>
2026-04-29 05:18:34 +0700

    refactor: streamline user management methods and improve response messages

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rule;
 
 class UserController extends Controller
@@ -32,118 +30,60 @@ class UserController extends Controller
 
     public function index(Request $request)
     {
-        $datas = $this->user
-            ->query()
-            ->when($request->query('search'), function ($query, $search) {
-                $query->where('name', 'like', "%$search%");
-                $query->orWhere('email', 'like', "%$search%");
-            })
-            ->paginate(request()->query('perPage', 10))
-            ->withQueryString()
-            ->onEachSide(3)
-            ->withPath('/user');
-        return compact(['datas']);
+        return $this->userRepository
+            ->paginate(
+                '/user',
+                $request->query('search'),
+                null,
+                $request->collect('orders'),
+            );
     }
 
     public function store(Request $request)
     {
         $validated = $request->validate([
-            'name' => ['required', 'string', 'max:255', 'min:3'],
-            'email' => ['required', 'email', 'max:255'],
-            'password' => ['required', 'string', 'max:255', 'min:8']
+            'name' => ['required', 'string', Rule::unique('users', 'name')],
+            'email' => ['required', 'string', 'email', Rule::unique('users', 'email')],
+            'password' => ['nullable', 'string', 'min:4', 'confirmed'],
         ]);
 
-        DB::beginTransaction();
+        $user = $this->userRepository->save($validated);
 
-        try {
-            $this->user->create($validated);
-
-            DB::commit();
-
-            return response()->json([
-                "message" => "The user was successfully created.",
-            ]);
-        } catch (\Throwable $th) {
-            DB::rollBack();
-
-            Log::error($th);
-
-            return response()->json([
-                "message" => "An error occurred while creating the user, contact admin."
-            ], 500);
-        }
+        return response()->json([
+            'message' => 'Your Data Successfully created',
+            'user' => $user,
+        ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(User $user)
     {
-        return compact(['user']);
+        return ['data' => $user];
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, User $user)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255', 'min:3', Rule::unique('users', 'name')->ignoreModel($user)],
-            'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email')->ignoreModel($user)],
-            'password' => ['nullable', 'string', 'max:255', 'min:8']
+        $validated = $request->validate([
+            'name' => ['required', 'string', Rule::unique('users', 'name')->ignoreModel($user)],
+            'email' => ['required', 'string', 'email', Rule::unique('users', 'email')->ignoreModel($user)],
+            'password' => ['nullable', 'string', 'min:4', 'confirmed'],
         ]);
 
-        DB::beginTransaction();
+        $validated['id'] = $user->id;
+        $user = $this->userRepository->save($validated);
 
-        try {
-            $user->fill($request->only(['name', 'email']));
-
-            if ($request->has('password')) {
-                $user->password = $request->input('password');
-            }
-
-            $user->save();
-
-
-            DB::commit();
-
-            return response()->json([
-                "message" => "The user was successfully updated.",
-            ]);
-        } catch (\Throwable $th) {
-            DB::rollBack();
-
-            Log::error($th);
-
-            return response()->json([
-                "message" => "An error occurred while updating the user, contact admin."
-            ], 500);
-        }
+        return response()->json([
+            'message' => 'Your Data Successfully updated',
+            'user' => $user,
+        ], 201);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(User $user)
     {
-        DB::beginTransaction();
+        $user->delete();
 
-        try {
-            $user->delete();
-
-            DB::commit();
-
-            return response()->json([
-                "message" => "The user was successfully deleted.",
-            ]);
-        } catch (\Throwable $th) {
-            DB::rollBack();
-
-            Log::error($th);
-
-            return response()->json([
-                "message" => "An error occurred while deleting the user, contact admin."
-            ], 500);
-        }
+        return response()->json([
+            'message' => 'Your Data Successfully deleted',
+            'user' => $user,
+        ]);
     }
 }
